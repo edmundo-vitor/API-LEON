17ec53b9c9897c81a86aae511e258665c6c17b3b
edmundo-vitor <edmundovitor1414@hotmail.com>
2021-10-15 15:18:03 -0300

    init

---
+++ src/test/java/br/com/leon/LeonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
