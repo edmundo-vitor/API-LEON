70ad032ab6ab8fe977ab3a6c7516298857668ca6
matheusgoes23 <matheusgoes23@gmail.com>
2021-10-16 17:36:50 -0300

    Created search for managers by paged permission

--- src/main/java/br/com/leon/repository/ManagerRepository.java
+++ src/main/java/br/com/leon/repository/ManagerRepository.java
@@ -1,7 +1,13 @@
 package br.com.leon.repository;
 
 import br.com.leon.model.Manager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+
+    @Query("SELECT DISTINCT obj FROM Manager obj WHERE obj.permission = UPPER(:permission)")
+    Page<Manager> findAllByPermissionPaged(String permission, Pageable pageable);
 }
