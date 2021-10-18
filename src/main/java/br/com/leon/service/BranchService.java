e6e6ef88714e05cb6b6f9c74b98de850daced12a
matheusgoes23 <matheusgoes23@gmail.com>
2021-10-18 11:16:59 -0300

    Created basic services: BranchService and NewsService

---
+++ src/main/java/br/com/leon/service/BranchService.java
@@ -0,0 +1,65 @@
+package br.com.leon.service;
+
+import br.com.leon.dto.BranchDTO;
+import br.com.leon.exceptions.DatabaseException;
+import br.com.leon.exceptions.NotFoundException;
+import br.com.leon.model.Branch;
+import br.com.leon.repository.BranchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class BranchService {
+
+    @Autowired
+    private BranchRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<BranchDTO> findAllPaged(Pageable pageable) {
+        Page<Branch> page = repository.findAll(pageable);
+        return page.map(BranchDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public BranchDTO findById(Long id) {
+        Optional<Branch> obj = repository.findById(id);
+        Branch entity = obj.orElseThrow(() -> new NotFoundException("Entity not found"));
+        return new BranchDTO(entity);
+    }
+
+    @Transactional
+    public BranchDTO save(BranchDTO dto) {
+        Branch entity = new Branch();
+        entity = repository.save(entity);
+        return new BranchDTO(entity);
+    }
+
+    @Transactional
+    public BranchDTO update(Long id, BranchDTO dto) {
+        try {
+            Branch entity = repository.getById(id);
+            entity = repository.save(entity);
+            return new BranchDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundException("Id " + id + " not found");
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NotFoundException("Id " + id + " not found");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+}
---
+++ src/main/java/br/com/leon/service/NewsService.java
@@ -0,0 +1,65 @@
+package br.com.leon.service;
+
+import br.com.leon.dto.NewsDTO;
+import br.com.leon.exceptions.DatabaseException;
+import br.com.leon.exceptions.NotFoundException;
+import br.com.leon.model.News;
+import br.com.leon.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class NewsService {
+
+    @Autowired
+    private NewsRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<NewsDTO> findAllPaged(Pageable pageable) {
+        Page<News> page = repository.findAll(pageable);
+        return page.map(NewsDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public NewsDTO findById(Long id) {
+        Optional<News> obj = repository.findById(id);
+        News entity = obj.orElseThrow(() -> new NotFoundException("Entity not found"));
+        return new NewsDTO(entity);
+    }
+
+    @Transactional
+    public NewsDTO save(NewsDTO dto) {
+        News entity = new News();
+        entity = repository.save(entity);
+        return new NewsDTO(entity);
+    }
+
+    @Transactional
+    public NewsDTO update(Long id, NewsDTO dto) {
+        try {
+            News entity = repository.getById(id);
+            entity = repository.save(entity);
+            return new NewsDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundException("Id " + id + " not found");
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NotFoundException("Id " + id + " not found");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+}
