c26cc55d236cc6f20d2a21a7ced7a468cc0ef016
matheusgoes23 <matheusgoes23@gmail.com>
2021-10-18 14:25:20 -0300

    Updated save and update methods in: BranchService and NewsService

--- src/main/java/br/com/leon/service/NewsService.java
+++ src/main/java/br/com/leon/service/NewsService.java
@@ -3,7 +3,9 @@ package br.com.leon.service;
 import br.com.leon.dto.NewsDTO;
 import br.com.leon.exceptions.DatabaseException;
 import br.com.leon.exceptions.NotFoundException;
+import br.com.leon.model.Manager;
 import br.com.leon.model.News;
+import br.com.leon.repository.ManagerRepository;
 import br.com.leon.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -22,6 +24,9 @@ public class NewsService {
     @Autowired
     private NewsRepository repository;
 
+    @Autowired
+    private ManagerRepository managerRepository;
+
     @Transactional(readOnly = true)
     public Page<NewsDTO> findAllPaged(Pageable pageable) {
         Page<News> page = repository.findAll(pageable);
@@ -38,6 +43,7 @@ public class NewsService {
     @Transactional
     public NewsDTO save(NewsDTO dto) {
         News entity = new News();
+        copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new NewsDTO(entity);
     }
@@ -46,6 +52,7 @@ public class NewsService {
     public NewsDTO update(Long id, NewsDTO dto) {
         try {
             News entity = repository.getById(id);
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new NewsDTO(entity);
         } catch (EntityNotFoundException e) {
@@ -61,5 +68,14 @@ public class NewsService {
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Integrity violation");
         }
+    }
+
+    private void copyDtoToEntity(NewsDTO dto, News entity) {
+
+        entity.setDescription(dto.getDescription());
+        entity.setDate(dto.getDate());
+
+        Manager manager = managerRepository.getById(dto.getManager().getId());
+        entity.setManager(manager);
     }
 }
