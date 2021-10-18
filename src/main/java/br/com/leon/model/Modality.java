7486a85d7d7234f310ca4e9b3b2bcd381d37b8d0
Lucas <araujolucas3005@gmail.com>
2021-10-18 15:41:31 -0300

    change name to not null

--- src/main/java/br/com/leon/model/Modality.java
+++ src/main/java/br/com/leon/model/Modality.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -13,7 +14,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
-
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
@@ -26,8 +26,9 @@ public class Modality {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false)
   private String name;
-  
+
   @Setter(AccessLevel.NONE)
   private Date createdAt;
 
@@ -36,7 +37,7 @@ public class Modality {
 
   @OneToMany(mappedBy = "modality", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   private Set<Schedule> schedules = new HashSet<>();
-  
+
   @PrePersist
   private void onPrePersist() {
     createdAt = updatedAt = new Date();
