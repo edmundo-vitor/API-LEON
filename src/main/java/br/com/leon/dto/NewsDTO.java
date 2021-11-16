0919a71326ea46ccdae86788ea452bef30eb1de0
matheusgoes23 <matheusgoes23@gmail.com>
2021-11-16 16:02:21 -0300

    Updated news functionalities

--- src/main/java/br/com/leon/dto/NewsDTO.java
+++ src/main/java/br/com/leon/dto/NewsDTO.java
@@ -2,8 +2,10 @@ package br.com.leon.dto;
 
 import br.com.leon.model.Manager;
 import br.com.leon.model.News;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -17,25 +19,36 @@ public class NewsDTO implements Serializable {
 
     private Long id;
 
+    @NotBlank(message = "Title cannot be empty")
+    private String title;
+
     @NotBlank(message = "Description cannot be empty")
     private String description;
 
+    @URL(message = "Image url cannot be valid")
+    private String imageUrl;
+
     @NotNull(message = "Date cannot be null")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date date;
 
     @NotNull(message = "Manager cannot be null")
     private ManagerDTO manager;
 
-    public NewsDTO(Long id, String description, Date date, ManagerDTO manager) {
+    public NewsDTO(Long id, String title, String description, String imageUrl, Date date, ManagerDTO manager) {
         this.id = id;
+        this.title = title;
         this.description = description;
+        this.imageUrl = imageUrl;
         this.date = date;
         this.manager = manager;
     }
 
     public NewsDTO(News entity) {
         this.id = entity.getId();
+        this.title = entity.getTitle();
         this.description = entity.getDescription();
+        this.imageUrl = entity.getImageUrl();
         this.date = entity.getDate();
     }
 
--- src/main/java/br/com/leon/model/News.java
+++ src/main/java/br/com/leon/model/News.java
@@ -9,13 +9,15 @@ import java.util.Date;
 @Entity
 @Table(name = "tb_news")
 @Data
-public class News implements Serializable {
+public class News extends BaseEntity implements Serializable {
     public static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String title;
     private String description;
+    private String imageUrl;
     private Date date;
 
     @ManyToOne
