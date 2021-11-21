7f9b56051e5f75bd5109545a81eb445e902e811e
matheusgoes23 <matheusgoes23@gmail.com>
2021-11-21 01:36:32 -0300

    Added authentication and authorization configuration

---
+++ src/main/java/br/com/leon/config/ResourceServerConfig.java
@@ -0,0 +1,89 @@
+package br.com.leon.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    public static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
+
+    //TODO implementar autorização com base nas na função de cada um
+
+    public static final String[] ADMIN = {"/branches/**", "/managers/**", "/users/**"};
+
+    public static final String[] MANAGER = {};
+
+    public static final String[] CLERK = {};
+
+    public static final String[] USER = {};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // H2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        //TODO definir authorização
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(ADMIN).permitAll()
+                .antMatchers(ADMIN).hasAnyRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
--- src/main/java/br/com/leon/service/ManagerService.java
+++ src/main/java/br/com/leon/service/ManagerService.java
@@ -3,21 +3,27 @@ package br.com.leon.service;
 import br.com.leon.dto.BranchDTO;
 import br.com.leon.dto.ManagerDTO;
 import br.com.leon.dto.ManagerInsertDTO;
+import br.com.leon.dto.RoleDTO;
 import br.com.leon.exceptions.DatabaseException;
 import br.com.leon.exceptions.NotFoundException;
+import br.com.leon.model.Authentication;
 import br.com.leon.model.Branch;
 import br.com.leon.model.Manager;
+import br.com.leon.model.Role;
 import br.com.leon.repository.BranchRepository;
 import br.com.leon.repository.ManagerRepository;
+import br.com.leon.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,35 +35,41 @@ public class ManagerService {
     @Autowired
     private BranchRepository branchRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
     public Page<ManagerDTO> findAllPaged(Pageable pageable) {
         Page<Manager> page = repository.findAll(pageable);
         return page.map(ManagerDTO::new);
     }
 
     @Transactional(readOnly = true)
-    public Page<ManagerDTO> findAllByPermissionPaged(String permission, Pageable pageable) {
-        Page<Manager> page = repository.findAllByPermissionPaged(permission, pageable);
-        return page.map(ManagerDTO::new);
+    public List<Role> findAllRoles() {
+        return roleRepository.findAll();
     }
 
     @Transactional(readOnly = true)
     public ManagerDTO findById(Long id) {
         Optional<Manager> obj = repository.findById(id);
         Manager entity = obj.orElseThrow(() -> new NotFoundException("Entity not found"));
-        return new ManagerDTO(entity, entity.getBranches());
+        return new ManagerInsertDTO(entity, entity.getBranches(), entity.getAuthentication());
     }
 
     @Transactional
     public ManagerDTO save(ManagerInsertDTO dto) {
         Manager entity = new Manager();
         copyDtoToEntity(dto, entity);
-        entity.setPassword(dto.getPassword());  //TODO criptografar senha quando usar o spring security
+        entity.getAuthentication().setPassword(passwordEncoder.encode(dto.getAuthentication().getPassword()));
         entity = repository.save(entity);
         return new ManagerDTO(entity, entity.getBranches());
     }
 
     @Transactional
-    public ManagerDTO update(Long id, ManagerDTO dto) {
+    public ManagerDTO update(Long id, ManagerInsertDTO dto) {
         try {
             Manager entity = repository.getById(id);
             copyDtoToEntity(dto, entity);
@@ -78,13 +90,20 @@ public class ManagerService {
         }
     }
 
-    private void copyDtoToEntity(ManagerDTO dto, Manager entity) {
+    private void copyDtoToEntity(ManagerInsertDTO dto, Manager entity) {
 
         entity.setName(dto.getName());
-        entity.setEmail(dto.getEmail());
         entity.setAddress(dto.getAddress());
         entity.setPhone(dto.getPhone());
-        entity.setPermission(dto.getPermission());
+
+        Authentication authentication = new Authentication();
+        authentication.setEmail(dto.getAuthentication().getEmail());
+
+        for (RoleDTO roleDto : dto.getAuthentication().getRoles()) {
+            Role role = roleRepository.getById(roleDto.getId());
+            authentication.getRoles().add(role);
+        }
+        entity.setAuthentication(authentication);
 
         entity.getBranches().clear();
         for (BranchDTO branchDTO : dto.getBranches()) {
